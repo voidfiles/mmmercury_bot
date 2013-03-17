de3c41ebba5bb0c3d17145f3675d3afbda53e385
Alex Kessinger <alex@mixedmedialabs.com>
2013-03-16 21:14:35 -0700

    Adding a setup.py

---
+++ setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+import mmmercury
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+packages = [
+    "mmmercury"
+]
+
+requires = [
+    "redis==2.7.2",
+    "requests==1.1.0",
+    "redisco==0.1.4",
+    "iso8601==0.1.4",
+    "-e git+git@github.com:saffsd/langid.py.git@5048323bd2f21265a06dcc0717ceb26bf05806f1#egg=langid-dev",
+    "pytz==2012j",
+]
+
+setup(
+    name='mmmercury',
+    version=mmmercury.__version__,
+    description='An App.net bot.',
+    long_description=open('README.md').read(),
+    author='Alex Kessinger',
+    author_email='voidfiles@gmail.com',
+    url='https://github.com/voidfiles/mmmercury_bot',
+    packages=packages,
+    package_data={},
+    package_dir={'mmmercury': 'mmmercury'},
+    include_package_data=True,
+    install_requires=requires,
+    license=open('LICENSE').read(),
+    zip_safe=False,
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ),
+)
